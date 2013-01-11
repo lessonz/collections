e411d5315aed246633e4db61d913b7624ee58c85
lessonz <devnull@localhost>
2013-01-10 21:19:39 -0700

    Added comments to DistanceFunction.

--- src/main/java/lessonz/collections/kdtree/distance/DistanceFunction.java
+++ src/main/java/lessonz/collections/kdtree/distance/DistanceFunction.java
@@ -1,7 +1,21 @@
 package lessonz.collections.kdtree.distance;
 
+/**
+ * A {@link DistanceFunction} provides the ability determine the distance between two k-dimensional points.
+ */
 public interface DistanceFunction {
 
-    double distance(double[] coordinateSet1, double[] coordinateSet2);
+	/**
+	 * Calculates the distance between two points. In order to provide the maximum performance possible many
+	 * implementation will <strong>not</strong> implement any kind of error checking such as ensuring the two specified
+	 * arrays have the same length.
+	 * 
+	 * @param coordinateSet1
+	 *            k-dimensional point where each value is the point's position in that plane.
+	 * @param coordinateSet2
+	 *            k-dimensional point where each value is the point's position in that plane.
+	 * @return the distance between the two points.
+	 */
+	double distance(double[] coordinateSet1, double[] coordinateSet2);
 
 }
